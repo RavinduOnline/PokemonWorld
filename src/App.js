fa0901a3c90fc6de0dbed4b179b76bfa08f353d6
RavinduOnline <ravindurasanga99@gmail.com>
2022-12-04 18:16:12 +0530

    set fetching data

--- src/App.js
+++ src/App.js
@@ -1,38 +1,76 @@
 import './App.css';
+import React , { useState, useEffect } from "react";
 import Card from './components/card'
 
 function App() {
 
-  const pokemon_count = 50;
-  const colors = {
-     fire: '#FDDFDF',
-     grass: '#DEFDE0',
-     electric: '#FCF7DE',
-     water: '#DEF3FD',
-     ground: '#f4e7da',
-     rock: '#d5d5d4',
-     fairy: '#fceaff',
-     poison: '#98d7a5',
-     bug: '#f8d5a3',
-     dragon: '#97b3e6',
-     psychic: '#eaeda1',
-     flying: '#F5F5F5',
-     fighting: '#E6E0D4',
-     normal: '#F5F5F5',
-  };
+  const [pokedata , setPokeData] = useState([]);  
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+
+ const getPokemons = async () => {
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=102`;
+
+    fetch(url).then(res=>res.json())
+        .then(response=>{
+          console.log(response);
+          setPokeData(response.results);
+      })
+      .catch((err)=>{
+          console.log("Err - ",err)
+      })
+ };
+
+
+//  const createPokemonCard = (pokemon) => {
+//     const pokemonEl = document.createElement('div');
+//     pokemonEl.classList.add('pokemon');
+ 
+//     const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+//     const id = pokemon.id.toString().padStart(3, '0');
+ 
+//     const poke_types = pokemon.types.map((type) => type.type.name);
+//     const type = pokemon_types.find((type) => poke_types.indexOf(type) > -1);
+//     const color = pokemon_types[type];
+ 
+//     pokemonEl.style.backgroundColor = color;
+ 
+//     const pokemonInnerHTML = `
+//      <div class="img-container">
+//          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png"" alt="${name}">
+//      </div>
+//      <div class="info">
+//          <span class="number">#${id}</span>
+//          <h3 class="name">${name}</h3>
+//          <small class="type">Type: <span>${type}</span> </small>
+//      </div>
+//      `;
+ 
+//     pokemonEl.innerHTML = pokemonInnerHTML;
+ 
+
+//  };
+ 
 
   return (
     <div>
       <div className='mt-16'></div>
       <div class="mx-36 grid grid-cols-6 gap-4">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+
+      { pokedata.map((pokemon ,index)=> {
+
+        return(
+          <div> 
+               <Card pokemon={pokemon} />
+          </div>
+        )
+
+        })}
+
+        
       </div>
       
     </div>
--- src/components/card.js
+++ src/components/card.js
@@ -1,21 +1,81 @@
-import React from 'react'
+import React , { useState, useEffect } from "react";
 
 
-export default function card() {
+export default function Card({pokemon} ) {
+
+  const pokemon_colors = {
+    fire: '#FF7070',
+    grass: '#82FF9D',
+    electric: '#FFD73E',
+    water: '#82C3FF',
+    ground: '#CFB18E',
+    rock: '#A08D77',
+    fairy: '#FF92E0',
+    poison: '#E39AFD',
+    bug: '#B3E383',
+    dragon: '#A0EEFF',
+    psychic: '#FFC2F5',
+    flying: '#8FA7A9',
+    fighting: '#FFAF82',
+    dark:'#A8A8A8',
+    ice:'#AAE5FF',
+    ghost:'#B8AECC',
+    steel:'#9ABFB6',
+    normal:'#AF95C3',
+ };
+
+  const [pokemonData , setPokemonData] = useState([]);
+  const [getpokemonType , setGetPokemonType] = useState([]);
+  const [url, setUrl] = useState('');
+  const [pokemonType , setPokemonType] = useState('');
+  const [color , setColor] = useState('');
+
+
+  useEffect(() => {
+    getPokemonData();
+  },[pokemon]);
+
+
+const getPokemonData = async () => {
+    const url = pokemon.url;
+
+    fetch(url).then(res=>res.json())
+        .then(response=>{
+          console.log(response);
+          setPokemonData(response);
+          setGetPokemonType(response.types)
+          setUrl(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${response.id}.png`);
+          getType();
+      })
+      .catch((err)=>{
+          console.log("Err - ",err)
+      })
+ };
+
+const getType = () => {
+
+    const poke_types = getpokemonType.map((type) => type.type.name);
+    setPokemonType(poke_types.find((type) => poke_types.indexOf(type) > -1));
+    const pokeType = poke_types.find((type) => poke_types.indexOf(type) > -1);
+    setColor(pokemon_colors[pokeType]);
+    console.log(pokeType);
+}
+
+
   return (
     <div>
         <div className='relative bg-white w-full rounded-lg overflow-hidden shadow-lg shadow-cyan-500/50'>  
 
-            <div  class="absolute h-40	w-full rounded-bl-full rounded-br-full" style={{backgroundColor: "#82C3FF"}} />
+            <div  class="absolute h-40	w-full rounded-bl-full rounded-br-full" style={{backgroundColor:color}} />
 
             <div className='h-56  flex  flex-col justify-end items-center'>
                   <div className='relative flex flex justify-center '>
-                        <img   className='w-2/3'  src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"/>   
+                        <img   className='w-2/3'  src={url} alt="Pokemon Img"/>   
                   </div>  
             </div>
-            <div className='mb-4 mt-4 w-full flex  flex-col justify-center items-center'>
-              <h1 className='text-3xl font-bold' >Gastly</h1>
-              <h3 className='text-md font-medium	'>Ghost</h3>
+            <div className='mb-4 mt-4 w-full flex  flex-col justify-center items-center' >
+              <h1 className='text-3xl font-bold' >{pokemonData.name}</h1>
+              <h3 className='text-md font-medium	'>{pokemonType}</h3>
             </div>
         </div>
 
