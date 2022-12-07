c64ac067e2715be93bdbef9ad08ae80d5bcc8329
RavinduOnline <ravindurasanga99@gmail.com>
2022-12-07 18:39:16 +0530

    version 1

--- src/App.js
+++ src/App.js
@@ -32,34 +32,35 @@ function App() {
   return (
     <div>
       <Header/>
-      <div className='xl:mt-44 mt-36'></div>
-      <div class="lg:mx-36 xl:mx-5 mb-5 mx-5">
-                <select name="cars" id="cars"
-                  value={count}
-                  onChange={(e) => setCount(e.target.value)}
-                  >
-                        <option value="102" selected  >102</option>
-                        <option value="50">50</option>
-                        <option value="250">250</option>
-                        <option value="500">500</option>
-                        <option value="750">750</option>
-                        <option value="1000">1000</option>
-                  </select>
-      </div>
-      <div class="md:mx-36 xl:mx-5  md:grid md:grid-cols-6 grid-cols-4 gap-4 mx-5">
+      <div className='mx-auto md:max-w-screen-xl xl:mt-44 mt-36'>
+            <div class=" lg:mx-36 xl:mx-5 mb-5 mx-5">
+                      <select name="cars" id="cars"
+                        value={count}
+                        onChange={(e) => setCount(e.target.value)}
+                        >
+                              <option value="102" selected  >102</option>
+                              <option value="50">50</option>
+                              <option value="250">250</option>
+                              <option value="500">500</option>
+                              <option value="750">750</option>
+                              <option value="1000">1000</option>
+                        </select>
+            </div>
+            <div class="lg:mx-36 xl:mx-5  md:grid  lg:grid-cols-5 xl:grid-cols-6 grid-cols-4 gap-4 mx-5">
 
-      { pokedata.map((pokemon ,index)=> {
+              { pokedata.map((pokemon ,index)=> {
 
-        return(
-          <div> 
-               <Card pokemon={pokemon} />
-          </div>
-        )
+                return(
+                  <div> 
+                      <Card pokemon={pokemon} index={index} />
+                  </div>
+                )
 
-        })}
+                })}
 
-        
-      </div>
+              
+            </div>
+        </div>
       
     </div>
   );
--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,7 @@
 import React , { useState, useEffect } from "react";
 
 
-export default function Card({pokemon} ) {
+export default function Card({pokemon , index} ) {
 
   const pokemon_colors = {
     fire: '#FF7070',
@@ -24,10 +24,32 @@ export default function Card({pokemon} ) {
     normal:'#AF95C3',
  };
 
+ const pokemon_Icon = {
+  fire: "/images/fire.png",
+  grass: '/images/grass.png',
+  electric: '/images/electric.png',
+  water: '/images/water.png',
+  rock: '/images/rock.png',
+  fairy: '/images/fairy.png',
+  poison: '/images/poison.png',
+  bug: '/images/bug.png',
+  dragon: '/images/dragon.png',
+  psychic: '/images/psychic.png',
+  flying: '/images/flying.png',
+  fighting: '/images/fighting.png',
+  dark:'/images/dark.png',
+  ice:'/images/ice.png',
+  ghost:'/images/ghost.png',
+  steel:'/images/steel.png',
+  normal:'/images/normal.png',
+};
+
   const [pokemonData , setPokemonData] = useState([]);
   const [url, setUrl] = useState('');
   const [pokemonType , setPokemonType] = useState('');
   const [color , setColor] = useState('');
+  const [icon , setIcon] = useState('');
+
 
 
   useEffect(() => {
@@ -55,24 +77,30 @@ const getType = (data) => {
     setPokemonType(poke_types.find((type) => poke_types.indexOf(type) > -1));
     const pokeType = poke_types.find((type) => poke_types.indexOf(type) > -1);
     setColor(pokemon_colors[pokeType]);
+    setIcon(pokemon_Icon[pokeType]);
 }
 
 
   return (
     <div>
-        <div className='mb-8  relative bg-white w-full rounded-lg overflow-hidden shadow-lg shadow-cyan-500/50'>  
+        <div className='mb-8 relative bg-white w-full rounded-lg overflow-hidden shadow-lg shadow-cyan-500/50'>  
 
-            <div  class="absolute md:h-28 lg:h-40 h-48	w-full rounded-bl-full rounded-br-full" style={{backgroundColor:color}} />
-            <div  class="absolute md:h-28 lg:h-40	h-48 w-full rounded-bl-full rounded-br-full opacity-5" style={{backgroundColor:"#000000"}} />
+            <div  class="absolute h-1/2	w-full rounded-bl-full rounded-br-full" style={{backgroundColor:color}} />
+            <div  class="absolute h-1/2 w-full rounded-bl-full rounded-br-full opacity-5 bg-black"/>
 
-            <div className='lg:h-56 md:h-36 h-80 flex  flex-col justify-end items-center'>
-                  <div className='relative flex flex justify-center '>
-                        <img   className='lg:w-2/3 w-3/6'  src={url} alt="Pokemon Img"/>   
+            <div  class="absolute mt-2 ml-3 text-white font-extrabold drop-shadow-md">#{index}</div>
+
+            <div className='mt-5 flex  flex-col justify-end items-center'>
+                  <div className='relative flex  justify-center '>
+                        <img   className='lg:w-2/3 w-3/6'  src={url} alt=""/>   
                   </div>  
             </div>
             <div className='mb-4 mt-4 w-full flex  flex-col justify-center items-center' >
-              <h1 className='text-3xl font-bold capitalize' >{pokemonData.name}</h1>
-              <h3 className='text-md font-medium	capitalize'>{pokemonType}</h3>
+              <h1 className='text-3xl font-bold capitalize text-center' >{pokemonData.name}</h1>
+              <div className="flex mt-1">
+                <img   className='w-5 h-5 justify-center items-center'  src={icon} alt=""/>   
+                <h3 className='ml-1 text-md font-medium	capitalize'>{pokemonType}</h3>
+              </div>
             </div>
         </div>
 
